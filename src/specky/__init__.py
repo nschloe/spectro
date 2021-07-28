a136c7cc71a074828aa5974f7fd49ff4a417c9f1
Nico Schlömer <nico.schloemer@gmail.com>
2021-07-28 12:54:50 +0200

    remove __about__

--- src/specky/__init__.py
+++ src/specky/__init__.py
@@ -1,10 +1,4 @@
 from . import cli
-from .__about__ import __version__
 from ._main import check, show
 
-__all__ = [
-    "__version__",
-    "cli",
-    "check",
-    "show",
-]
+__all__ = ["cli", "check", "show"]
